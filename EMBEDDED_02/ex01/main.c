5fbc921a0bcefbc74d69bb5be596bf8a69088ed0
Lounis Abdellou <lounisabdellou.dev@gmail.com>
2025-03-05 18:55:23 +0100

    opk

--- EMBEDDED_02/ex01/main.c
+++ EMBEDDED_02/ex01/main.c
@@ -5,23 +5,41 @@
 #define FCPU 16000000
 #define BAUD 115200
 #define UBRR FCPU / 16 / BAUD
+#define BUFFER_SIZE 128
+
+uint8_t readPos;
+uint8_t writePos;
+char buffer[BUFFER_SIZE];
 
 void uart_init(void) {
   UBRR0H = (UBRR >> 8);
   UBRR0L = UBRR;
-  UCSR0B = (1 << TXEN0);
+  UCSR0B = (1 << TXEN0) | (1 << TXCIE0);
   UCSR0C = (1 << UCSZ00) | (1 << UCSZ01);
 }
 
-void uart_tx(void) {
-  UDR0 = 'Z';
-  _delay_ms(1000);
+void append(char c) {
+  buffer[writePos++] = c;
+
+  if (writePos >= BUFFER_SIZE) {
+    writePos = 0;
+  }
+}
+
+void write(char c[]) {}
+
+void uart_printstr() {
+  // "Hello World!\r\n"
 }
 
 int main(void) {
   uart_init();
+  sei();
 
   while (1) {
-    uart_tx();
+    /* uart_printstr(); */
+    /* _delay_ms(2000); */
   }
 }
+
+ISR(USART_TX_vect) {}
